256a38eb40c473b76b8d13f39d1596b23d4a0c12
Yhanis Boullonnois <yhanis@app-stack.tech>
2025-12-30 22:44:11 +0100

    feat: add job question and continue button to onboarding job page

--- app/src/features/onboarding/pages/OnboardingJobPage.tsx
+++ app/src/features/onboarding/pages/OnboardingJobPage.tsx
@@ -1,13 +1,37 @@
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Text from "../../../shared/ui/text";
+import { useRouter } from "expo-router";
+import Input from "../../../shared/ui/input";
+import { colors, gaps } from "../../../shared/constants/theme";
+import Button from "../../../shared/ui/button";
 
 export const OnboardingJobPage = () => {
+  const router = useRouter();
+
+  const handleContinue = () => {
+    router.push("/(onboarding)/(steps)/signup");
+  };
 
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text variant="h2" weight="bold">{"Second, \nWhat's your job ?"}</Text>
+      </View>
+      <Button onPress={handleContinue}>
+        <Text variant="body" weight="bold" color={colors.white}>Continue</Text>
+      </Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: gaps.default,
+    justifyContent: 'space-between',
+  },
+  content: {
+    flex: 1,
+    gap: gaps.default,
+  },
 });
-
